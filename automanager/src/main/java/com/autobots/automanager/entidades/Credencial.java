50e69a938bdf0a7373483c67888840e45a4515d9
Kaue-Francisco <kauesantos.francisco@gmail.com>
2024-11-29 20:20:21 -0300

    feat: Adicionando a lista 3.

---
+++ automanager/src/main/java/com/autobots/automanager/entidades/Credencial.java
@@ -0,0 +1,66 @@
+package com.autobots.automanager.entidades;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import lombok.Data;
+
+
+@JsonTypeInfo(
+		use = JsonTypeInfo.Id.NAME,
+		include = JsonTypeInfo.As.PROPERTY,
+		property = "tipo"
+)
+@JsonSubTypes({
+		@JsonSubTypes.Type(value = CredencialUsuarioSenha.class, name = "usuarioSenha")
+})
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Credencial extends RepresentationModel<Credencial>{
+	@Id()
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private Date criacao;
+	@Column()
+	private Date ultimoAcesso;
+	@Column(nullable = false)
+	private boolean inativo;
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Date getCriacao() {
+		return criacao;
+	}
+	public void setCriacao(Date criacao) {
+		this.criacao = criacao;
+	}
+	public Date getUltimoAcesso() {
+		return ultimoAcesso;
+	}
+	public void setUltimoAcesso(Date ultimoAcesso) {
+		this.ultimoAcesso = ultimoAcesso;
+	}
+	public boolean isInativo() {
+		return inativo;
+	}
+	public void setInativo(boolean inativo) {
+		this.inativo = inativo;
+	}
+}
